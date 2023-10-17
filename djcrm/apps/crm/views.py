e398085f27024e687699e2665b84f4443ff540d9
jarthurj <jaoldaker1@gmail.com>
2023-10-16 20:38:33 -0400

    commiter

--- djcrm/apps/crm/views.py
+++ djcrm/apps/crm/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
 	context = {
-	
 	}
-	return render(request, "home.html", context)
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, "Login Succesful")
+			return redirect("home")
+		else:
+			messages.success(request, "Login Error")
+			return redirect("home")
+	else:
+		return render(request, "home.html", context)
+
+def login_user(request):
+	pass
+
+def logout_user(request):
+	pass
